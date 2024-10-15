537ed457ec6f243b5a410bd925fc1cd4ac6b857f
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 10:39:48 +0600

    java datatypes

---
+++ Java/src/datatypes/dataTypes.java
@@ -0,0 +1,48 @@
+package datatypes;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        System.out.println("datatypes in java");
+        /*
+        There are 2 types of datatypes in java
+        1. primitive
+            1. byte
+            2. short
+            3. int
+            4. float
+            5. double
+            6. char
+            7. boolean
+            8. long
+        2. non-primitive
+            1. Array.
+            2. Strings.
+            3. Class.
+         */
+
+//        1. byte
+        byte extraMiniNum = 127;
+        System.out.println(extraMiniNum);
+//        2. short
+        short miniNum = 32767;
+        System.out.println(miniNum);
+//        3. int
+        int num = 320000000;
+        System.out.println(num);
+//        4. float
+        float height = 5.514f;
+        System.out.println(height);
+//        5. double
+        double weight = 414.51452405145d;
+        System.out.println(weight);
+//        6. char
+        char singleChar = 'c';
+        System.out.println(singleChar);
+//        7. boolean
+        boolean isAuthenticated= false;
+        System.out.println(isAuthenticated);
+//        8. long
+        long dinoNum = 62505204051455404L;
+        System.out.println(dinoNum);
+    }
+}
