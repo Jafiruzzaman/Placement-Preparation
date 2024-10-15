6295e38f55243c107a7a47730d665170bf1836e3
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 10:30:17 +0600

    add variables

---
+++ Java/src/variables/variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args) {
+        System.out.println("variables and rules while creating variables");
+    }
+}
+/*
+
+variables: variables are called container to store a values.
+In another definition, variables are called the title of a reversed memory location.
+
+rules while creating variables:
+1. variables name must start with letter,dollar sign($) or underscore(_)
+2. variables name can't start with number or any other special character
+3. variables name can't be a java reserved keyword.
+4. variables name are case-sensitive.
+5. variables name must be meaningful
+
+ */
