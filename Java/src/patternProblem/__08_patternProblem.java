b49cf6d8497f62f578098819b314c209bfc2626e
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 13:02:37 +0600

    solve probem 08

---
+++ Java/src/patternProblem/__08_patternProblem.java
@@ -0,0 +1,13 @@
+package patternProblem;
+
+public class __08_patternProblem {
+    public static void main(String[] args) {
+        System.out.println("find the sum of the following series");
+        int n = 10;
+        int result = 0;
+        for (int i=1;i<=10;i++){
+            result+=i;
+        }
+        System.out.println("The Sum of following series is "+result);
+    }
+}
