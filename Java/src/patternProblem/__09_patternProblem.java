b59d78de7782c3aab251eb3705224506989b9e8a
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 13:32:03 +0600

    solve problem 09

--- Java/src/patternProblem/__09_patternProblem.java
+++ Java/src/patternProblem/__09_patternProblem.java
@@ -1,4 +1,13 @@
 package patternProblem;
 
 public class __09_patternProblem {
+    public static void main(String[] args) {
+        System.out.println("find the factorial of the given number");
+        int num = 5;
+        int fact = 1;
+        for (int i = 1;i<=num;i++){
+            fact*=i;
+        }
+        System.out.println("The factorial of "+num+" is "+fact);
+    }
 }
