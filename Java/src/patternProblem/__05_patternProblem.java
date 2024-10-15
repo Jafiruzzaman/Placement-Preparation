c00901f4286752a7cfb32723edc5225328fff9d5
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 12:47:18 +0600

    solve problem 05

---
+++ Java/src/patternProblem/__05_patternProblem.java
@@ -0,0 +1,16 @@
+package patternProblem;
+
+public class __05_patternProblem {
+    public static void main(String[] args) {
+        System.out.println("Count the number of digit's for given number");
+        int num = 1785; // output --> 1+7+8+5 = 21
+        int result = 0;
+        while(num>0){
+//            get the last digit from num
+            int lastDigit = num%10;
+            result+=lastDigit;
+            num/=10;
+        }
+        System.out.println("The result is "+result);
+    }
+}
