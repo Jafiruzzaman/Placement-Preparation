e1c8b63c41c7c3d96a9244535bd44c54c60ccf00
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 12:59:04 +0600

    solve problem 07

--- Java/src/patternProblem/__07_patternProblem.java
+++ Java/src/patternProblem/__07_patternProblem.java
@@ -1,4 +1,15 @@
 package patternProblem;
 
 public class __07_patternProblem {
+    public static void main(String[] args) {
+        System.out.println("reverse the digit of a number");
+        int num = 1785; // output --> 5871
+        int answer = 0;
+        while(num>0){
+            int lastDigit = num%10;
+            answer  = (answer *10)+lastDigit ;
+            num/=10;
+        }
+        System.out.println("The reverse number is "+answer);
+    }
 }
