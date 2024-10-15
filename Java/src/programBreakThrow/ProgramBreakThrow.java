0b77dcfad27311b6a7c43f64659ba80e36982e97
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 10:19:22 +0600

    add Program Break Throw

---
+++ Java/src/programBreakThrow/ProgramBreakThrow.java
@@ -0,0 +1,29 @@
+package programBreakThrow;
+// This is single line comment
+/*
+This is multi
+line
+comment
+
+*/
+public class ProgramBreakThrow {
+    public static void main(String[] args) {
+        System.out.println("java program break throw");
+    }
+}
+
+/*
+
+package : package is a collection of subpackages,classes and objects
+public: public is an access modifier.
+class: class is a user defined datatype
+ProgramBreakThrow: It is always same with the file name it's public class name.
+static: static allows to call methods without creating classes.
+void: void is the return type.void means null.
+main: main is the name of the function.
+(Strings[]): it's command line argument's
+System: it's a reserved class in java for Standard Input and Output Stream
+out: out means for output.
+println: print in a new line.
+
+*/
