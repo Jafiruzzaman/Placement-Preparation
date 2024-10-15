e4bb815c6869057ebaf52b3112202f96e98f9146
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-10-15 15:22:31 +0600

    basics of strings

---
+++ Strings/src/BasicString.java
@@ -0,0 +1,11 @@
+public class BasicString {
+  public static void main(String[] args) {
+    System.out.println("basic string");
+    // create a new string
+    // There are two different ways to create a string
+    String naam = "Tuhin Ahamed";
+    System.out.println(naam);
+    String name = new String("Tuhin Ahamed");
+    System.out.println(name);
+  }
+}
